be831a3886394a308c468e963adb0b7f26746c6c
MAC VALIA <macvalia@MacBook-Pro-de-MAC.local>
2018-12-04 11:59:59 -0200

    changed app text

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-           Conrad atuando com React! Esse é o Rei!
+           Conrad atuando com React! Esse é o Rei alterando um branch de release simulando merge de outro branch com outro engenheiro do time!
           </p>
           <a
             className="App-link"
